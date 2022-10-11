c72f60cbba98bf5f61b02115ce04ceba19232a35
gmlukario <gmlukario@gmail.com>
2022-10-10 23:48:31 -0500

    🚧: some app constants

---
+++ src/constants.rs
@@ -0,0 +1,14 @@
+// Labels
+pub const WINDOW_TITTLE: &str = "Pos Software";
+
+// Buttons labels
+pub const CATALOG_BTN_MSG: &str = "Catálogo";
+pub const SALE_BTN_MSG: &str = "Venta";
+pub const SALES_INFO_BTN_MSG: &str = "Estadísticas";
+pub const TO_BUY_BTN_MSG: &str = "Lista Compra";
+
+// Sizes
+pub const SIZE_TEXT: u16 = 40;
+pub const SIZE_BTNS_TEXT: u16 = 30;
+pub const SPACE_ROWS: u16 = 10;
+pub const SPACE_COLUMNS: u16 = 10;
