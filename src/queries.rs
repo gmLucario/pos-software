ce63766aa4b3e9bc7180414606c6bbf57aaef0f6
gmlukario <gmlukario@gmail.com>
2022-10-21 14:32:44 -0500

    🗃️: queries to get sale info

--- src/queries.rs
+++ src/queries.rs
@@ -43,3 +43,24 @@ limit 1;
 pub const INSERT_PRODUCT_CATALOG: &str = r#"
 call sp_save_product_catalog($1, $2, $3, $4, $5, $6, $7);
 "#;
+
+pub const GET_SALE_PRODUCT_INFO: &str = r#"
+select
+    product.barcode,
+	product.full_name as product_name,
+	product.user_price as price,
+	product.unit_measurement_id,
+	1::numeric(5,3) as amount,
+	product.unit_measurement_id,
+	sum("catalog".current_amount) as total_amount
+from product
+left join "catalog" on (
+	product.id = "catalog".product_id
+)
+where product.barcode = $1
+group by
+	product.barcode,
+	product.full_name,
+	product.user_price,
+	product.unit_measurement_id;
+"#;
