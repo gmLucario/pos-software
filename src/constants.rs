ed80c778b20fe8ea9c3297ec8e2b97eee6bed4e1
gmlukario <gmlukario@gmail.com>
2022-10-21 14:31:49 -0500

    🚧: new constants

--- src/constants.rs
+++ src/constants.rs
@@ -16,3 +16,7 @@ pub const SIZE_TEXT_INPUT: u16 = 25;
 pub const SIZE_BTNS_TEXT: u16 = 30;
 pub const SPACE_ROWS: u16 = 10;
 pub const SPACE_COLUMNS: u16 = 10;
+
+// Data
+pub const TO_DECIMAL_DIGITS: i64 = 2;
+pub const MAX_CONNECTIONS_POOL: u32 = 2;
