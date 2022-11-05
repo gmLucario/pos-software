64f328c2ea071990a161d73a98365977792207b1
gmlukario <gmlukario@gmail.com>
2022-11-04 23:28:33 -0600

    🧐: loan model

--- src/models/sale.rs
+++ src/models/sale.rs
@@ -5,7 +5,10 @@ use sqlx::{
     types::{BigDecimal, Uuid},
 };
 
-use crate::{constants::PGMONEY_DECIMALS, schemas::sale::SaleInfo};
+use crate::{
+    constants::{PGMONEY_DECIMALS, TO_DECIMAL_DIGITS},
+    schemas::sale::SaleInfo,
+};
 
 #[derive(sqlx::FromRow, Debug, Clone)]
 pub struct SaleProductInfo {
@@ -52,6 +55,32 @@ impl From<&SaleInfo> for Sale {
         Self {
             client_payment,
             products: barcodes,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SaleLoan {
+    pub sale_id: Uuid,
+    pub name_debtor: String,
+    pub money_amount: PgMoney,
+    pub is_valid: bool,
+}
+
+impl From<&SaleInfo> for SaleLoan {
+    fn from(schema: &SaleInfo) -> Self {
+        let client_payment = BigDecimal::from_str(&schema.client_pay).unwrap();
+        let client_payment = PgMoney::from_bigdecimal(client_payment, PGMONEY_DECIMALS).unwrap();
+
+        let zero_amount = BigDecimal::default();
+
+        let money_amount = schema.total_pay - client_payment;
+        let is_valid = money_amount.to_bigdecimal(TO_DECIMAL_DIGITS) > zero_amount;
+        Self {
+            sale_id: Uuid::default(),
+            name_debtor: schema.client_name.to_string(),
+            money_amount,
+            is_valid,
         }
     }
 }
