aaa1bdf3ac5b5c6b7ed3bbf9662bd7480402d2e3
gmlukario <gmlukario@gmail.com>
2022-11-10 21:54:19 -0600

    🔥: remove states widgets to buy controller

--- src/controllers/to_buy.rs
+++ src/controllers/to_buy.rs
@@ -1,16 +1,12 @@
 //! Handle logic to link [`crate::views::to_buy`]
 //! module with the [`crate::data::product_repo::ProductRepo`]
 
-use iced::scrollable;
-
-use crate::models::catalog::ProductsToBuy;
+use crate::models::catalog::ProductToBuy;
 
 /// Controller links [`crate::views::to_buy`] module with the [`crate::data::product_repo::ProductRepo`]
 /// and perform the logic of products to be bought to fill the stock
 #[derive(Default)]
 pub struct ToBuy {
     /// List products to be bought
-    pub products: Vec<ProductsToBuy>,
-    /// Iced scrollable state
-    pub scroll_list_state: scrollable::State,
+    pub products: Vec<ProductToBuy>,
 }
