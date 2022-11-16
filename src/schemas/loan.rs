7bf12a2ac383157a24418ec95b198fd21f16aaaa
Gerardo Miguel <gmlukario@gmail.com>
2022-11-15 23:28:29 -0600

    📝: schema docs (#18)
    * 📝: sale schema docs
    
    * 📝: loan schema docs
    
    * 📝: catalog schema docs
    
    * 📝: update readme

--- src/schemas/loan.rs
+++ src/schemas/loan.rs
@@ -1,8 +1,14 @@
+//! Data structures to get user inputs related to loan info view
+
 use iced_aw::date_picker::Date;
 
+/// Represents user input values
 pub struct LoanSchema {
+    /// Date start for lookig loans
     pub start_date: Date,
+    /// Until this date to lookig loans
     pub end_date: Date,
+    /// Client name like, to looking loans
     pub client: String,
 }
 
@@ -17,8 +23,11 @@ impl Default for LoanSchema {
     }
 }
 
+/// Represents states of the date picker
 #[derive(Default)]
 pub struct LoanDatePickerStates {
+    /// If start_date picker is shown or not
     pub show_start_date: bool,
+    /// If end_date picker is shown or not
     pub show_end_date: bool,
 }
--- src/schemas/sale.rs
+++ src/schemas/sale.rs
@@ -1,3 +1,5 @@
+//! Data structures to get user inputs related to sale view
+
 use std::{collections::HashMap, str::FromStr};
 
 use sqlx::{postgres::types::PgMoney, types::BigDecimal};
@@ -8,23 +10,32 @@ use crate::{
     models,
 };
 
+/// Represents input values to define a new product to be sold
 #[derive(Default, Debug, Clone)]
 pub struct ProductToAdd {
+    /// `barcode` of the product
     pub barcode: String,
+    /// Full name of the product
     pub product_name: String,
+    /// Price to be charged to the client
     pub price: String,
+    /// Unit of measurement of the product: kg, lts or pieces
     pub unit_measurement: UnitsMeasurement,
+    /// Amount/quantity of the product
     pub amount: String,
+    /// Amount of the product in the stock
     pub total_amount: BigDecimal,
 }
 
 impl ProductToAdd {
+    /// Reset the struct field values
     pub fn reset_values(&mut self) {
         *self = Self::default();
     }
 }
 
 impl From<models::sale::SaleProductInfo> for ProductToAdd {
+    /// Cast from the model to the schema
     fn from(model: models::sale::SaleProductInfo) -> Self {
         let unit_measurement = UnitsMeasurement::from(model.unit_measurement_id);
 
@@ -39,14 +50,19 @@ impl From<models::sale::SaleProductInfo> for ProductToAdd {
     }
 }
 
+/// Represents values to be shown in the sale list
 #[derive(Debug, Clone)]
 pub struct ProductList {
+    /// full name of the product
     pub product_name: String,
+    /// amount/quantity of the pruduct
     pub amount: BigDecimal,
+    /// price to be charged per unit
     pub price: PgMoney,
 }
 
 impl From<&ProductToAdd> for ProductList {
+    /// Cast from user input values schema to list item schema
     fn from(schema: &ProductToAdd) -> Self {
         let amount = BigDecimal::from_str(&schema.amount).unwrap();
         let price = BigDecimal::from_str(&schema.price).unwrap();
@@ -60,12 +76,18 @@ impl From<&ProductToAdd> for ProductList {
     }
 }
 
+/// Represents the main sale info
 #[derive(Debug, Clone)]
 pub struct SaleInfo {
+    /// Products to be sold
     pub products: HashMap<String, ProductList>,
+    /// Total money client will pay
     pub total_pay: PgMoney,
+    /// Money client payed
     pub client_pay: String,
+    /// Client full name if it was a loan
     pub client_name: String,
+    /// Money to give client back
     pub payback_money: PgMoney,
 }
 
