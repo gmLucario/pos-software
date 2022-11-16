8513c1ca78d9a851f05d802381956ea072ad1d3c
gmlukario <gmlukario@gmail.com>
2022-11-15 22:43:23 -0600

    🚚: rename font icons

--- src/views/fonts.rs
+++ src/views/fonts.rs
@@ -3,7 +3,7 @@
 use iced::Font;
 
 /// Garbage icon to be used in the app
-pub const GARBAGE_ICON: Font = Font::External {
+pub const FONT_ICONS: Font = Font::External {
     name: "Icons",
     bytes: include_bytes!("../../assets/fonts/icons.ttf"),
 };
